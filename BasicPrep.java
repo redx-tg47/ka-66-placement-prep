8d574bd4014131923d3500aeb466d259745b002f
redx-tg47 <pgithub47@gmail.com>
2025-08-19 14:52:17 +0530

    [19-08-25] diff approaches of  factorialNumber

--- BasicPrep.java
+++ BasicPrep.java
@@ -1,8 +1,13 @@
+import java.util.stream.IntStream;
+
 public class BasicPrep {
 
   public static void main(String[] args) {
     fibonacciSeries(10); // O(n) and O(1)
     reverseNumber(-123); // O(log10(n)) and O(1)
+    factorialNumber(5); // O(n) and O(1)
+    factorialNumberRecursive(5); // O(n) and O(n) due to recursion stack space
+    factorialNumberStream(5); // O(n) and O(1) for the stream operation
   }
 
   // What is fibonacciSeries?
@@ -38,5 +43,37 @@ public class BasicPrep {
     reverse *= isNegative; // Restore the sign if it was negative
     System.out.println("reversed number " + reverse);
   }
+
+  public static void factorialNumber(int n) {
+    System.out.println("factorial for number " + n);
+    int sum = 1;
+    for(int i = 1; i <= n ; i++){
+      sum *= i;
+    }
+    System.out.println("factorial is " + sum);
+  }
+
+  // using recursion
+  public static void factorialNumberRecursive(int n) {
+    System.out.println("factorial for number " + n);
+    int result = factorialRecursive(n);
+    System.out.println("factorial is " + result);
+  }
+
+  private static int factorialRecursive(int n) {
+    if (n <= 1) {
+      return 1;
+    }
+    return n * factorialRecursive(n - 1); // n * (n-1)
+  }
+
+  // using stream
+   public static void factorialNumberStream(int n) {
+     System.out.println("factorial for number " + n);
+     int result = IntStream.rangeClosed(1, n)
+       .reduce(1, (a, b) -> a * b);
+     System.out.println("factorial is " + result);
+   }
+
 
 }
